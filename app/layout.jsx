0d62548549801b57750198b0a7ce361eb7ea683e
dalienst <oduordalienst@gmail.com>
2025-07-15 10:22:20 +0300

    add landing

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,25 @@
 "use client";
 
 import "./globals.css";
+import TanstackQueryProvider from "@/providers/TanstackQueryProvider";
+import { Analytics } from "@vercel/analytics/react";
+import { Toaster } from "react-hot-toast";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <title>Sherehe</title>
+        <meta
+          name="description"
+          content="Discover Amazing Events. Find and book tickets for the most exciting events in your area"
+        />
+      </head>
+      <body>
+        <Toaster position="top-center" />
+        <TanstackQueryProvider>{children}</TanstackQueryProvider>
+        <Analytics />
+      </body>
     </html>
   );
 }
